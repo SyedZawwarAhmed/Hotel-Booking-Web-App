d65eff61098156f2a71f270c34a73efe7ee86005
SyedZawwarAhmed <zawwar.ahmed12@gmail.com>
2022-03-20 00:16:05 +0500

    user model added

---
+++ models/user.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const userModel = mongoose.model("users", userSchema);
+
+module.exports = userModel;
