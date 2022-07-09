73eed138dfaa498508ff57636a1c3c638d1c2429
SyedZawwarAhmed <zawwar.ahmed12@gmail.com>
2022-07-09 19:12:13 +0500

    nav dropdown added

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,9 +1,43 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import { DownOutlined } from "@ant-design/icons";
+import { Dropdown, Menu, Space } from "antd";
 import { Link } from "react-router-dom";
 import "../stylesheets/Navbar.css";
 
 function Navbar() {
   const currentUser = JSON.parse(localStorage.getItem("currentUser"));
+  
+  let menu;
+  if (currentUser) {
+    menu = (
+      <Menu
+        items={[
+          {
+            label: "Sign out",
+            onClick: removeCurrentUser,
+            key: "0",
+          },
+          {
+            label: <Link to="/user/profile">Profile</Link>,
+            key: "1",
+          },
+          {
+            type: "divider",
+          },
+          currentUser.isAdmin && (
+          {
+            label: (
+              <Link to="/user/admin">
+                Admin
+              </Link>
+            ),
+            key: "3",
+          }),
+        ]}
+      />
+    );
+  }
+
 
   function removeCurrentUser() {
     console.log("user removed");
@@ -20,24 +54,33 @@ function Navbar() {
           </Link>
         </div>
         {currentUser ? (
-          <div className="nav-buttons">
-            <h1 className="user-name">{currentUser.name}</h1>
-            <button
-              className="btn nav-btn signout-btn"
-              onClick={removeCurrentUser}
-            >
-              Signout
-            </button>
-            <Link to="/user/profile" className="btn nav-btn profile-btn">
-              Profile
-            </Link>
-            {currentUser.isAdmin && (
-              <Link to="/user/admin" className="btn nav-btn profile-btn">
-                Admin
-              </Link>
-            )}
-          </div>
+          
+          <Dropdown className="nav-buttons profile-dropdown" overlay={menu} trigger={["click"]}>
+            <a onClick={(e) => e.preventDefault()}>
+              <Space>
+              <h1 className="user-name">{currentUser.name}</h1>
+                <DownOutlined />
+              </Space>
+            </a>
+          </Dropdown>
         ) : (
+          //   <div className="nav-buttons">
+          //     <h1 className="user-name">{currentUser.name}</h1>
+          //     <button
+          //       className="btn nav-btn signout-btn"
+          //       onClick={removeCurrentUser}
+          //     >
+          //       Signout
+          //     </button>
+          //     <Link to="/user/profile" className="btn nav-btn profile-btn">
+          //       Profile
+          //     </Link>
+          //     {currentUser.isAdmin && (
+          //       <Link to="/user/admin" className="btn nav-btn profile-btn">
+          //         Admin
+          //       </Link>
+          //     )}
+          //   </div>
           <div className="nav-buttons">
             <Link to="/user/signup" className="btn nav-btn">
               Singup
