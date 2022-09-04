be05f399e98d331d37724009f7fd22c2971f8925
SyedZawwarAhmed <zawwar.ahmed12@gmail.com>
2022-09-04 14:56:02 +0500

    update api.js

--- src/api.js
+++ src/api.js
@@ -4,19 +4,16 @@ const serverless = require("serverless-http");
 const app = express();
 const dbConfig = require("./db");
 const roomsRoute = require("./routes/roomsRoute");
-const usersRoute = require("./routes/usersRoute")
-const bookingsRoute = require("./routes/bookingsRoute")
+const usersRoute = require("./routes/usersRoute");
+const bookingsRoute = require("./routes/bookingsRoute");
 
-var cors = require('cors')
+var cors = require("cors");
 
-app.use(cors())
-app.use(express.json())
-app.use("/rooms", roomsRoute);
-app.use("/users", usersRoute)
-app.use("/bookings", bookingsRoute)
+app.use(cors());
+app.use(express.json());
+app.use("/.netlify/functions/api/rooms", roomsRoute);
+app.use("/.netlify/functions/api/users", usersRoute);
+app.use("/.netlify/functions/api/bookings", bookingsRoute);
 
-const port = process.env.PORT || 5000;
-
-app.use(`/.netlify/functions/api`, router);
 module.exports = app;
-module.exports.handler = serverless(app)
+module.exports.handler = serverless(app);
