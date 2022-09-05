df46dba0f9fc1ec6cda85c67e97e499e12dd217d
SyedZawwarAhmed <zawwar.ahmed12@gmail.com>
2022-09-05 22:38:09 +0500

    update Room.js

--- client/src/components/Room.js
+++ client/src/components/Room.js
@@ -10,14 +10,18 @@ function Room({ _id, name, images, type, toDate, fromDate, rentperday }) {
         <h1 className="room-name">{name}</h1>
         <h3>{type}</h3>
         <h3 className="rent">Rent per day:- Rs. {rentperday}</h3>
-        {fromDate && toDate && (
-          <Link
-            className="btn book-btn"
-            to={`/booking/${_id}/${fromDate}/${toDate}`}
-          >
-            Book Now
-          </Link>
-        )}
+        {fromDate &&
+          toDate &&
+          (JSON.parse(localStorage.getItem("currentUser")) ? (
+            <Link
+              className="btn book-btn"
+              to={`/booking/${_id}/${fromDate}/${toDate}`}
+            >
+              Book Now
+            </Link>
+          ) : (
+            <h3 className="signin-reminder">Sign in to book</h3>
+          ))}
       </div>
     </div>
   );
